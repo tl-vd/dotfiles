b5b2f4d1f252a1bcbe00de6f40d4b0457c75c36d
tl-vd <mb@tl-vd.net>
2015-03-01 05:27:37 +0100

    formatting

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,13 +1,12 @@
-# Set prompt
 
-PROMPT="%F{yellow}%{${fg[gray]}%}%3~%(0?. . ${fg[red]}%? )%{${reset_color}%}%F{white}| %(?.%F{magenta}.%F{red})>%f " # Display a red prompt char on failure
-# ❯❯
+# Prompt
+PROMPT="%F{yellow}%{${fg[gray]}%}%3~%(0?. . ${fg[red]}%? )%{${reset_color}%}%F{white}| %(?.%F{magenta}.%F{red})>%f "
 
 # Variables
 export MEDIA='/chest/media'
 export DOWNLOADS='/chest/downloads'
 
-# Load modules
-source /home/markus/.zsh_modules/misc/aliases.zsh
-source /home/markus/.zsh_modules/misc/functions.zsh
-source /home/markus/.zsh_modules/*.zsh
+# Modules
+source $HOME/.zsh_modules/misc/aliases.zsh
+source $HOME/.zsh_modules/misc/functions.zsh
+source $HOME/.zsh_modules/*.zsh
